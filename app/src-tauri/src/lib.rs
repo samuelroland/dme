a194bb6ab35dba3580f209fa25568bf18f0d472f
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-04-02 01:32:36 +0200

    Add basic load_markdown_as_html() in Rust using comrak

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,14 +1,28 @@
+use std::fs::read_to_string;
+
+use comrak::{markdown_to_html, Options};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn load_markdown_as_html(path: &str) -> Result<String, String> {
+    // Some("salut !".to_string())
+    read_to_string(path)
+        .as_ref()
+        .map(|c| convert_md_to_html(c))
+        .map_err(|_| "Failed to load file".to_string())
+}
+
+fn convert_md_to_html(raw: &str) -> String {
+    let mut options = Options::default();
+    options.extension.table = true;
+    markdown_to_html(raw, &options)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![load_markdown_as_html])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
