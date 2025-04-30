6a424414951c2ff21491dd09c5984f8bed832c9e
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-04-30 22:39:55 +0200

    Add 2 basics Tauri commands get_file_to_show() and get_app_info()

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,6 +1,9 @@
 use std::fs::read_to_string;
 mod highlight;
 
+use serde::Serialize;
+use tauri::{AppHandle, Emitter};
+
 use comrak::adapters::SyntaxHighlighterAdapter;
 use comrak::nodes::{AstNode, NodeValue};
 use comrak::{
@@ -10,8 +13,30 @@ use comrak::{
 use highlight::TreeSitterHighlighter;
 use inkjet::{formatter, Highlighter, Language};
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[derive(Serialize)]
+struct AppInfo {
+    version: String,
+}
+
 #[tauri::command]
+fn get_app_info() -> AppInfo {
+    AppInfo {
+        version: env!("CARGO_PKG_VERSION").to_owned(),
+    }
+}
+
+#[tauri::command]
+fn get_file_to_show() -> Option<Result<String, String>> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        None
+    } else {
+        let file = &args[1];
+        Some(load_markdown_as_html(file))
+    }
+}
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 fn load_markdown_as_html(path: &str) -> Result<String, String> {
     // Some("salut !".to_string())
     read_to_string(path)
@@ -38,7 +63,7 @@ fn convert_md_to_html(raw: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![load_markdown_as_html])
+        .invoke_handler(tauri::generate_handler![get_file_to_show, get_app_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
