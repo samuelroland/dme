9ba7f54fe2dc81282681af279a998e8385a5778b
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-06 18:18:59 +0200

    Decide on TREE_SITTER_GRAMMARS_FOLDER in tree_sitter module, add install(), update(), and delete()

--- app/core/src/preview/tree_sitter.rs
+++ app/core/src/preview/tree_sitter.rs
@@ -4,18 +4,22 @@
 // and the associated implementation
 // https://dotat.at/cgi/git/wwwdotat.git/blob/HEAD:/src/hilite.rs
 
+use etcetera::{AppStrategy, AppStrategyArgs};
 use once_cell::sync::Lazy;
 use regex::Regex;
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     fs::{self, read_to_string, File},
+    hash::Hash,
+    iter::Map,
     os::fd::{AsFd, AsRawFd},
     path::{Path, PathBuf},
     process::{exit, Command},
     str::FromStr,
 };
+use tree_sitter::Language;
 use tree_sitter_highlight::{Highlight, HighlightConfiguration, Highlighter, HtmlRenderer};
-use tree_sitter_loader::{CompileConfig, Loader};
+use tree_sitter_loader::{CompileConfig, Config, Loader};
 
 use crate::util::git::GitRepos;
 
@@ -23,6 +27,21 @@ use super::preview::Html;
 
 // TODO: refactor with dynamic path from configuration
 const HIGHLIGHT_QUERIES_PATH: &str = "queries/highlights.scm";
+
+static HIGHLIGHT_NAMES_PARSER_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"\@[A-Za-z\.]+").unwrap());
+
+/// Use the DATA HOME strategy to determine the base folder grammars and cloned and managed
+/// on Linux it will be under ~/.local/share/tree-sitter-grammars
+static TREE_SITTER_GRAMMARS_FOLDER: Lazy<PathBuf> = Lazy::new(|| {
+    etcetera::choose_app_strategy(AppStrategyArgs {
+        app_name: "tree-sitter-grammars".to_string(),
+        ..Default::default()
+    })
+    .unwrap()
+    .data_dir()
+    .to_path_buf()
+});
 
 /// A highlighter for a specific language, once loaded it can highlight multiple code snippets of
 /// the same programming language
@@ -56,10 +75,11 @@ impl<'a> TreeSitterHighlighter<'a> {
             read_to_string(grammar_directory.join(HIGHLIGHT_QUERIES_PATH)).unwrap();
 
         let mut highlight_names_in_queries: HashSet<&str> = HashSet::new();
-        static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\@[A-Za-z\.]+").unwrap());
-        RE.find_iter(&highlight_queries_content).for_each(|n| {
-            highlight_names_in_queries.insert(&n.as_str()[1..]);
-        });
+        HIGHLIGHT_NAMES_PARSER_REGEX
+            .find_iter(&highlight_queries_content)
+            .for_each(|n| {
+                highlight_names_in_queries.insert(&n.as_str()[1..]);
+            });
         highlight_names_in_queries
             .into_iter()
             .collect::<Vec<String>>()
@@ -142,21 +162,57 @@ pub(crate) struct TreeSitterGrammarsManager {
 }
 
 impl<'a> TreeSitterGrammarsManager {
-    fn install(git_repo_url: &'a str) -> Result<(), String> {
-        let loader = Loader::new();
-        GitRepos::from_clone(git_repo_url)?;
-        loader.compile_parser_at_path(grammar_path, output_path, flags)
+    /// Create a new manager with a loader that needs a Tree-Sitter LIBDIR
+    pub fn new() -> Result<Self, String> {
+        let loader = Loader::new().map_err(|e| e.to_string())?;
+        Ok(TreeSitterGrammarsManager { loader })
     }
 
-    fn remove(lang: &'a str) -> Result<(), String> {
-        todo!()
+    /// Install a new grammar from a given git link
+    pub fn install(&self, git_repo_url: &'a str) -> Result<(), String> {
+        let repos = GitRepos::from_clone(git_repo_url, &*TREE_SITTER_GRAMMARS_FOLDER)?;
+        self.loader.compile_parser_at_path(
+            &repos.path(),
+            repos.path().clone(),
+            Vec::default().as_slice(),
+        );
+        Ok(())
     }
 
-    fn proposed_grammar_sources() -> Vec<(String, String)> {
-        Vec::from(PROPOSED_GRAMMAR_SOURCES)
-            .iter()
-            .map(|g| (g.0.to_string(), g.1.to_string()))
-            .collect()
+    /// Update the grammar behind the given lang
+    pub fn update(&self, lang: &'a str) -> Result<(), String> {
+        let repos = Self::get_repos_for_lang(lang)?;
+        repos.pull()?;
+        self.compile_at_path(repos.path())?;
+        Ok(())
+    }
+
+    /// Delete the grammar behind the given lang
+    /// This is consuming self to avoid reusing it after deletion
+    pub fn delete(self, lang: &'a str) -> Result<(), String> {
+        let repos = Self::get_repos_for_lang(lang)?;
+        std::fs::remove_dir_all(repos.path())
+            .map(|_| ())
+            .map_err(|e| e.to_string())
+    }
+
+    /// Helper to quickly get the repository behind the lang
+    fn get_repos_for_lang(lang: &str) -> Result<GitRepos, String> {
+        GitRepos::from_existing_folder(
+            TREE_SITTER_GRAMMARS_FOLDER.join(format!("tree-sitter-{}", lang)),
+        )
+    }
+
+    fn compile_at_path(&self, repos_path: &PathBuf) -> Result<Language, String> {
+        let src_path = repos_path.join("src");
+        // No output path, let it take the default in TREE_SITTER_LIBDIR
+        let config = CompileConfig::new(&src_path, None, None);
+        self.loader
+            .load_language_at_path(config)
+            .map_err(|e| e.to_string())
+
+        // Note: Do not use loader.compile_parser_at_path because it forces us to
+        // decide on the output file, we is not trivial to generate
     }
 
     /// Retrieve a list of languages accessible by Tree-Sitter
@@ -176,57 +232,59 @@ impl<'a> TreeSitterGrammarsManager {
 }
 
 /// This is shown by the UI as proposed default links from the tree-sitter and tree-sitter-grammars Github organisations
-const PROPOSED_GRAMMAR_SOURCES: [(&str, &str); 22] = [
-    (
-        "yaml",
-        "https://github.com/tree-sitter-grammars/tree-sitter-yaml",
-    ),
-    (
-        "lua",
-        "https://github.com/tree-sitter-grammars/tree-sitter-lua",
-    ),
-    (
-        "make",
-        "https://github.com/tree-sitter-grammars/tree-sitter-make",
-    ),
-    (
-        "toml",
-        "https://github.com/tree-sitter-grammars/tree-sitter-toml",
-    ),
-    (
-        "vue",
-        "https://github.com/tree-sitter-grammars/tree-sitter-vue",
-    ),
-    (
-        "csv",
-        "https://github.com/tree-sitter-grammars/tree-sitter-csv",
-    ),
-    (
-        "xml",
-        "https://github.com/tree-sitter-grammars/tree-sitter-xml",
-    ),
-    ("cpp", "https://github.com/tree-sitter/tree-sitter-cpp"),
-    ("php", "https://github.com/tree-sitter/tree-sitter-php"),
-    ("rust", "https://github.com/tree-sitter/tree-sitter-rust"),
-    ("scala", "https://github.com/tree-sitter/tree-sitter-scala"),
-    ("css", "https://github.com/tree-sitter/tree-sitter-css"),
-    ("regex", "https://github.com/tree-sitter/tree-sitter-regex"),
-    ("html", "https://github.com/tree-sitter/tree-sitter-html"),
-    ("java", "https://github.com/tree-sitter/tree-sitter-java"),
-    ("bash", "https://github.com/tree-sitter/tree-sitter-bash"),
-    (
-        "typescript",
-        "https://github.com/tree-sitter/tree-sitter-typescript",
-    ),
-    ("json", "https://github.com/tree-sitter/tree-sitter-json"),
-    ("go", "https://github.com/tree-sitter/tree-sitter-go"),
-    (
-        "haskell",
-        "https://github.com/tree-sitter/tree-sitter-haskell",
-    ),
-    ("c", "https://github.com/tree-sitter/tree-sitter-c"),
-    (
-        "javascript",
-        "https://github.com/tree-sitter/tree-sitter-javascript",
-    ),
-];
+pub static PROPOSED_GRAMMAR_SOURCES: Lazy<HashMap<&'static str, &'static str>> = Lazy::new(|| {
+    HashMap::from([
+        (
+            "yaml",
+            "https://github.com/tree-sitter-grammars/tree-sitter-yaml",
+        ),
+        (
+            "lua",
+            "https://github.com/tree-sitter-grammars/tree-sitter-lua",
+        ),
+        (
+            "make",
+            "https://github.com/tree-sitter-grammars/tree-sitter-make",
+        ),
+        (
+            "toml",
+            "https://github.com/tree-sitter-grammars/tree-sitter-toml",
+        ),
+        (
+            "vue",
+            "https://github.com/tree-sitter-grammars/tree-sitter-vue",
+        ),
+        (
+            "csv",
+            "https://github.com/tree-sitter-grammars/tree-sitter-csv",
+        ),
+        (
+            "xml",
+            "https://github.com/tree-sitter-grammars/tree-sitter-xml",
+        ),
+        ("cpp", "https://github.com/tree-sitter/tree-sitter-cpp"),
+        ("php", "https://github.com/tree-sitter/tree-sitter-php"),
+        ("rust", "https://github.com/tree-sitter/tree-sitter-rust"),
+        ("scala", "https://github.com/tree-sitter/tree-sitter-scala"),
+        ("css", "https://github.com/tree-sitter/tree-sitter-css"),
+        ("regex", "https://github.com/tree-sitter/tree-sitter-regex"),
+        ("html", "https://github.com/tree-sitter/tree-sitter-html"),
+        ("java", "https://github.com/tree-sitter/tree-sitter-java"),
+        ("bash", "https://github.com/tree-sitter/tree-sitter-bash"),
+        (
+            "typescript",
+            "https://github.com/tree-sitter/tree-sitter-typescript",
+        ),
+        ("json", "https://github.com/tree-sitter/tree-sitter-json"),
+        ("go", "https://github.com/tree-sitter/tree-sitter-go"),
+        (
+            "haskell",
+            "https://github.com/tree-sitter/tree-sitter-haskell",
+        ),
+        ("c", "https://github.com/tree-sitter/tree-sitter-c"),
+        (
+            "javascript",
+            "https://github.com/tree-sitter/tree-sitter-javascript",
+        ),
+    ])
+});
