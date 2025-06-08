2ae4684ab34ab983fdc60fed2bf5a893bd1b4fb6
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-08 11:23:50 +0200

    fix(preview): start testing install() and avoid failing on double install

--- app/core/src/preview/tree_sitter_grammars.rs
+++ app/core/src/preview/tree_sitter_grammars.rs
@@ -45,13 +45,25 @@ impl<'a> TreeSitterGrammarsManager {
         Ok(TreeSitterGrammarsManager { loader })
     }
 
-    /// Install a new grammar from a given git link
-    pub fn install(&self, git_repo_url: &'a str) -> Result<(), String> {
-        let repos = GitRepos::from_clone(git_repo_url, &TREE_SITTER_GRAMMARS_FOLDER)?;
+    /// Install a new grammar from a given git HTTPS URL
+    pub fn install(&mut self, git_repo_https_url: &'a str) -> Result<(), String> {
+        // Only clone the repository if it is not already present
+        // Note: we consider 2 repositories with the name folder name to be equivalent for now
+        let repos_name =
+            GitRepos::validate_and_extract_repos_name_from_https_url(git_repo_https_url)?;
+        let repos =
+            match GitRepos::from_existing_folder(&TREE_SITTER_GRAMMARS_FOLDER.join(&repos_name)) {
+                Ok(repos) => repos,
+                Err(_) => GitRepos::from_clone(git_repo_https_url, &TREE_SITTER_GRAMMARS_FOLDER)?,
+            };
+
+        self.loader.force_rebuild(true);
         self.loader
             .compile_parser_at_path(
                 repos.path(),
-                repos.path().clone(),
+                repos.path().clone().join(repos_name + ".so"),
+                // TODO: this
+                // will only work on Linux...
                 Vec::default().as_slice(),
             )
             .map_err(|e| e.to_string())?;
@@ -95,17 +107,83 @@ impl<'a> TreeSitterGrammarsManager {
     }
 
     /// Retrieve a list of languages accessible by Tree-Sitter
-    fn list_installed_langs(&self) -> Result<Vec<String>, String> {
+    pub fn list_installed_langs(&mut self) -> Result<Vec<String>, String> {
+        let mut config = Config::default();
+        config
+            .parser_directories
+            .push(TREE_SITTER_GRAMMARS_FOLDER.clone());
+        self.loader
+            .find_all_languages(&config)
+            .map_err(|e| e.to_string())?;
+
         Ok(self
             .loader
             .get_all_language_configurations()
             .iter()
             .map(|lc| lc.0.language_name.clone())
-            .collect::<Vec<String>>())
+            .collect())
     }
 
-    // Make sure local dependencies are installed, such as a GCC and git
-    fn check_local_deps(git_repo_url: &'a str) -> Result<(), String> {
-        todo!()
+    /// Make sure local dependencies are installed, such as a GCC and git
+    /// Returns Ok if all good or an Err with a reason
+    pub fn check_local_deps() -> Result<(), String> {
+        Command::new("gcc")
+            .arg("--version")
+            .output()
+            .map_err(|_| "Gcc not installed or not available in PATH".to_string())?;
+
+        if !GitRepos::is_git_installed() {
+            return Err("Git is not installed".to_string());
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{env::current_dir, fs::create_dir_all, path::PathBuf};
+
+    use crate::preview::{
+        proposed_grammars::PROPOSED_GRAMMAR_SOURCES,
+        tree_sitter_grammars::TreeSitterGrammarsManager,
+    };
+
+    fn get_test_grammar_repos() -> String {
+        PROPOSED_GRAMMAR_SOURCES.get("css").unwrap().to_string()
+    }
+
+    fn get_unique_local_tree_sitter_grammars_folder() -> PathBuf {
+        let base = current_dir()
+            .unwrap()
+            .join("target")
+            .join("tree-sitter-grammars");
+        let random: u32 = rand::random_range(0..=1000000000);
+        let unique_folder = base.join(random.to_string());
+        if !unique_folder.exists() {
+            create_dir_all(&unique_folder).expect("Couldn't create tests folder inside target/");
+        }
+        unique_folder
+    }
+
+    #[test]
+    fn test_can_install_csv_grammar() {
+        // Configure another grammars installation folder to avoid impacting the dev environnement
+        std::env::set_var(
+            "TREE_SITTER_GRAMMARS_FOLDER",
+            get_unique_local_tree_sitter_grammars_folder(),
+        );
+        let mut m = TreeSitterGrammarsManager::new().unwrap();
+        assert!(m.list_installed_langs().unwrap().is_empty());
+        let result = m.install(&get_test_grammar_repos());
+        dbg!(&result);
+        result.unwrap();
+    }
+    #[test]
+    fn test_check_local_deps() {
+        assert!(TreeSitterGrammarsManager::check_local_deps().is_ok());
+
+        std::env::set_var("PATH", ""); // empty the PATH so git will not be found
+        assert!(TreeSitterGrammarsManager::check_local_deps().is_err());
     }
 }
