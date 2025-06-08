d704873dd637406df64a5cef3af77fe654da07dc
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-08 20:26:59 +0200

    feat(core): add basic high level markdown_to_highlighted_html()

--- app/core/src/lib.rs
+++ app/core/src/lib.rs
@@ -1,7 +1,20 @@
+use std::fs::read_to_string;
+
+use preview::{
+    comrak::ComrakParser,
+    preview::{Html, Previewable},
+    tree_sitter_highlight::TreeSitterHighlighter,
+};
+
 pub mod preview;
 pub mod util;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-pub fn load_markdown_as_html(path: &str) -> Result<String, String> {
-    Ok("TODO".to_string())
+/// Given a Markdown file convert it to a full Html document that can be used as a .html file
+/// directly, with all the Tre-Sitter highlighting for code blocks and a default code theme applied
+pub fn markdown_to_highlighted_html(path: &str) -> Result<Html, String> {
+    let content = read_to_string(path)
+        .map_err(|e| "Couldn't find given file: ".to_string() + &e.to_string())?;
+    let parser = ComrakParser::new()?;
+    let html = parser.to_html(&content);
+    Ok(html)
 }
