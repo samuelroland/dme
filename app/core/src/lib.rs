e4fef9c13552e5e0ec96736a4ae4606d3da6b2d6
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-09 16:40:23 +0200

    feat(preview): include CSS generation in markdown_to_highlighted_html()

--- app/core/src/lib.rs
+++ app/core/src/lib.rs
@@ -10,13 +10,23 @@ use preview::{
     preview::{Html, Previewable},
     tree_sitter_highlight::TreeSitterHighlighter,
 };
+use theming::{
+    helix::ALL_HIGHLIGHT_NAMES_SUPPORTED_BY_HELIX,
+    renderer::{self, Renderer},
+    theme::{Theme, DEFAULT_THEME},
+};
 
 /// Given a Markdown file convert it to a full Html document that can be used as a .html file
 /// directly, with all the Tre-Sitter highlighting for code blocks and a default code theme applied
 pub fn markdown_to_highlighted_html(path: &str) -> Result<Html, String> {
     let content = read_to_string(path)
         .map_err(|e| "Couldn't find given file: ".to_string() + &e.to_string())?;
+    let theme = Theme::from_helix(DEFAULT_THEME, ALL_HIGHLIGHT_NAMES_SUPPORTED_BY_HELIX)
+        .map_err(|e| e.to_string())?;
+    let renderer = Renderer::new(&theme);
+    let css = renderer.css();
     let parser = ComrakParser::new()?;
     let html = parser.to_html(&content);
-    Ok(html)
+    let html = format!("<style>{}</style>\n\n{}", css, html.as_string());
+    Ok(Html(html))
 }
