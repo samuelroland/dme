43f66c45e55cad6a5d02d57dd4565c833cec4c24
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-09 14:51:47 +0200

    feat(ui): improvement in Search.vue comments, better events listening, remove logs, reformat

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,51 +5,72 @@ import { onKeyStroke } from '@vueuse/core'
 import { ref, onMounted } from "vue";
 import type { Ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
+import { ResearchResult } from "./types";
 
 export type AppInfo = {
-    version: string
+	version: string
 }
 const mdcontent = ref(null);
 const appInfo: Ref<AppInfo> = ref({ version: "??" });
 const lastPathUsed = ref(null)
 
-async function openMarkdown(path: string | null) {
-    lastPathUsed.value = path
-    try {
-        const result = await invoke("open_markdown_file", {path: path ?? ""}) as string;
-        mdcontent.value = result
-        return true
-    } catch (err) {
-        mdcontent.value = "<h2 class='text-red-300'>" + err + "</h2>"
-        return false
-    }
+// Open a Markdown file from disk, send null if unknown by the frontend yet
+// It's possible to give a heading to select once opened (useful during search)
+// Returns true if it has been opened, false in case of error
+async function openMarkdown(path: string | null, selectedHeading: string | null) {
+	lastPathUsed.value = path
+	try {
+		const result = await invoke("open_markdown_file", { path: path ?? "" }) as string;
+		mdcontent.value = result
+
+		// Try to scroll to the selected heading if present, after a small timeout
+		// to let the content to render, no headings are found otherwise
+		if (selectedHeading) {
+			setTimeout(() => {
+				const allHeadings = document.querySelectorAll('.prose h1, .prose h2, .prose h3, .prose h4, .prose h5, .prose h6') as unknown as HTMLElement[]
+				for (const heading of allHeadings) {
+					if (heading.innerText == selectedHeading) {
+						heading.scrollIntoView({ behavior: 'smooth', block: 'start' });
+					}
+				}
+			}, 50)
+		}
+		return true
+	} catch (err) {
+		mdcontent.value = "<h2 class='text-red-300'>" + err + "</h2>"
+		return false
+	}
+}
+
+async function openSearchEntry(entry: ResearchResult) {
+	return openMarkdown(entry.path, entry.title)
 }
 
 async function getAppInfo() {
-    const result = await invoke("get_app_info");
-    if (result != null) {
-        appInfo.value = result as AppInfo
-    }
+	const result = await invoke("get_app_info");
+	if (result != null) {
+		appInfo.value = result as AppInfo
+	}
 }
 
 onMounted(() => {
-    getAppInfo()
-    openMarkdown(lastPathUsed.value)
-    onKeyStroke(['r'], () => {
-        openMarkdown(lastPathUsed.value)
-    }
-    // Forced reload manually
- )
-    // TODO: remove this hacky polling based watch mode
-    setInterval(() => {let lastPath = lastPathUsed.value; openMarkdown(lastPath)}, 3000)
+	getAppInfo()
+	openMarkdown(lastPathUsed.value, null)
+	onKeyStroke(['r'], () => {
+		openMarkdown(lastPathUsed.value, null)
+	}
+		// Forced reload manually
+	)
+	// TODO: remove this hacky polling based watch mode
+	setInterval(() => { let lastPath = lastPathUsed.value; openMarkdown(lastPath, null) }, 3000)
 })
 </script>
 
 <template>
-    <div class="flex justify-center" v-if="mdcontent == null">
-        <Home :appInfo="appInfo" />
-    </div>
-    <article v-if="mdcontent != null" class="prose prose-base sm:prose-base md:prose-lg prose-zinc max-w-full
+	<div class="flex justify-center" v-if="mdcontent == null">
+		<Home :appInfo="appInfo" />
+	</div>
+	<article v-if="mdcontent != null" class="prose prose-base sm:prose-base md:prose-lg prose-zinc max-w-full
 	prose-h1:!mt-2
 	prose-h2:!mt-3
 	prose-h3:!mt-3
@@ -87,8 +108,8 @@ onMounted(() => {
 	prose-pre:whitespace-pre-wrap
 	selection:bg-blue-100 selection:text-black justify-center flex">
 
-        <div v-html="mdcontent" class="m-auto p-2 sm:m-5 md:m-10 lg:my-10 lg:mx-40 max-w-[1300px]">
-        </div>
-    </article>
-    <Search :openMarkdown="openMarkdown"/>
+		<div v-html="mdcontent" class="m-auto p-2 sm:m-5 md:m-10 lg:my-10 lg:mx-40 max-w-[1300px]">
+		</div>
+	</article>
+	<Search :openSearchEntry="openSearchEntry" />
 </template>
--- app/src/Search.vue
+++ app/src/Search.vue
@@ -3,21 +3,17 @@
 import { invoke } from '@tauri-apps/api/core';
 import { onKeyStroke } from '@vueuse/core'
 import { ref, onMounted, Ref } from "vue";
+import { ResearchResult } from './types';
 
 interface Props {
-  openMarkdown: (path: string|null) => any
+    openSearchEntry: (e: ResearchResult) => Promise<boolean>
 }
 const props = defineProps<Props>()
+
+// State of this search modal
 const searchOpened = ref(false);
-
-type ResearchResult = {
-    path: string,
-    title: string | null,
-    priority: number
-}
-
 const searchInput = ref(null)
-const results: Ref<ResearchResult[]> = ref([])
+const searchResults: Ref<ResearchResult[]> = ref([])
 const search = ref("")
 const error = ref("")
 const selectedEntry = ref(0)
@@ -25,66 +21,97 @@ const isFocused = ref(false)
 
 async function runSearch() {
     try {
-        const result = await invoke("run_search", {search: search.value}) as ResearchResult[];
-        results.value = result
+        const result = await invoke("run_search", { search: search.value }) as ResearchResult[];
+        if (searchResults.value != result) {
+            selectedEntry.value = 0 // reset selection entry on each results change
+        }
+        searchResults.value = result
         error.value = ""
     } catch (err) {
         error.value = err
     }
 }
 
-function escapeOnInput(){
+// If it is focused, we just want to loose focus, otherwise we want to close the search
+function escapeFromSearchInput() {
     if (isFocused.value) {
         searchInput.value?.blur()
         isFocused.value = false
     } else {
         searchOpened.value = false
-    }   
+    }
 }
- async function onEnter() {
-    const new_path = results.value[selectedEntry.value].path
-    if (await props.openMarkdown(new_path)) {
+
+// When pressing enter during search, we want to open the selected entry,
+// by default this is the first one
+async function onEnterDuringSearch() {
+    if (!searchOpened.value) return;
+
+    const entry = searchResults.value[selectedEntry.value]
+    const new_path = entry.path
+    if (new_path && new_path != "" && await props.openSearchEntry(entry)) {
         searchOpened.value = false
     }
 }
-function onKeyDown(e) {
-    if (e.key == 'Escape') escapeOnInput();
-    else if (e.key == 'Enter') onEnter();
+
+// React to enter, escape or any other key that trigger running the search again
+function onKeyDownOnSearchInput(e) {
+    if (e.key == 'Escape') escapeFromSearchInput();
+    else if (e.key == 'Enter') onEnterDuringSearch();
     else runSearch()
 }
+
+// Change the selected entry by incrementing the internal counter
+function changeSelectedEntry(increment: number) {
+    if (isFocused.value) return;
+    // Check bounds defined by length of the results
+    if (increment > 0 && selectedEntry.value < searchResults.value.length || selectedEntry.value > 0) {
+        selectedEntry.value = selectedEntry.value + increment
+    }
+}
+
+function insertSearchInputDuringSearch(e) {
+    if (isFocused.value || !searchOpened.value) return;
+    searchInput.value?.focus()
+    e.preventDefault() // to avoid entering "i" in the input
+}
+
+function onEntryClick(result: ResearchResult) {
+    if (props.openSearchEntry(result)) searchOpened.value = false
+}
+
 onMounted(() => {
+    // Toggle this search modal on key p
     onKeyStroke(['p'], () => {
-        console.log("isFocused", isFocused.value)
         searchOpened.value = true
         searchInput.value?.focus() // try to focus, even if this is not always successful
     })
-    onKeyStroke(['j'], () => {
-        console.log("isFocused", isFocused.value)
-        if (!isFocused.value) {
-            if (selectedEntry.value < 20) {
-                selectedEntry.value = selectedEntry.value + 1
-            }
+
+    // Define up and down actions on selection
+    onKeyStroke(['j', 'ArrowDown'], (e) => {
+        if (searchOpened.value) {
+            e.preventDefault()
+            changeSelectedEntry(1)
         }
     })
-    onKeyStroke(['k'], () => {
-        console.log("isFocused", isFocused.value)
-        if (!isFocused.value) {
-            if (selectedEntry.value > 0) {
-                selectedEntry.value = selectedEntry.value - 1
-            }
+    onKeyStroke(['k', 'ArrowUp'], (e) =>{
+        if (searchOpened.value) {
+            e.preventDefault()
+            changeSelectedEntry(-1)
         }
     })
-   
-    onKeyStroke(['Enter'], onEnter)
-    onKeyStroke(['Escape'], () => {
-      escapeOnInput()
-    })
+
+    // Declare escape and enter listener globally too to catch it when the output is not focused
+    onKeyStroke(['Enter'], onEnterDuringSearch)
+    onKeyStroke(['Escape'], escapeFromSearchInput)
+    onKeyStroke(['i'], insertSearchInputDuringSearch)
 })
 
-const MAX_SIZE = 60;
-function short(text: string) {
-    if (text.length > MAX_SIZE) {
-        return "..." + text.substring(text.length - MAX_SIZE)
+// Only show the end of the paths when too long to be able to see the filenames
+const MAX_PATH_SIZE = 60;
+function cutLongPathAtLeft(text: string) {
+    if (text.length > MAX_PATH_SIZE) {
+        return "..." + text.substring(text.length - MAX_PATH_SIZE)
     }
 }
 
@@ -92,30 +119,34 @@ function short(text: string) {
 
 <template>
     <div>
-    <div v-if="searchOpened"
-        class="fixed flex items-center justify-center inset-0 border bg-gray-200/40 overflow-hidden">
-        <div class="prose p-5 max-w-[90vh] max-h-[60vh] w-full h-full border rounded-sm  bg-gray-100/80 overflow-hidden">
-            <h2>Search</h2>
-            <input @focusin="isFocused = true"
-                v-model="search" ref="searchInput"
-                tabindex="1"
-                @keydown.stop="onKeyDown"
-                class="text-xl focus:bg-orange-300 w-full rounded-sm p-3 border border-orange-300"
-                placeholder="Starting typing file names or heading keywords..." autofocus />
-            {{ search }}
-            
-            <div v-if="error != ''" class="text-red-400">{{ error }}</div>
-            
-            <div v-for="(result, idx) in results">
-                <div :class="idx == selectedEntry ?'bg-orange-300/70': 'cursor-pointer hover:bg-orange-300/60 bg-orange-300/20' " :key="result.path" class="my-1 p-1 w-full overflow-hidden"
-                @click="() => {openMarkdown(result.path); searchOpened = false}">
-                    <div class="flex justify-between mx-3">
-                        <span v-if="result.title != null" class="font-bold">{{ result.title }}</span>
-                        <span class="italic">{{ short(result.path) }} <span class="ml-2 font-bold not-italic">{{ result.priority }}</span></span>
+        <div v-if="searchOpened"
+            class="fixed flex items-center justify-center inset-0 border bg-gray-200/40 overflow-hidden">
+            <div
+                class="p-5 max-w-[90vh] max-h-[60vh] w-full h-full border rounded-sm  bg-gray-100/80 overflow-hidden">
+                <h2 class="text-3xl font-bold">Search</h2>
+                <input @focusin="isFocused = true" v-model="search" ref="searchInput" tabindex="1"
+                    @keydown.stop="onKeyDownOnSearchInput"
+                    class="text-2xl font-bold focus:bg-orange-100 w-full rounded-sm p-3 border border-orange-300"
+                    placeholder="Starting typing file names or heading keywords..." autofocus />
+
+                <div v-if="error != ''" class="text-red-400">{{ error }}</div>
+
+                <div v-for="(result, idx) in searchResults">
+                    <div :class="idx == selectedEntry ? 'bg-orange-300/70' : 'cursor-pointer hover:bg-orange-300/60 bg-orange-300/20'"
+                        :key="result.path" class="my-1 p-1 w-full overflow-hidden" @click="() => onEntryClick(result)">
+                        <div class="flex justify-between mx-3">
+                            <span v-if="result.title != null" class="font-bold text-xl">{{ result.title }}</span>
+                            <span class="italic">{{ cutLongPathAtLeft(result.path) }}
+                                <span class="ml-2 font-bold not-italic">{{ result.priority }}</span>
+                            </span>
+                        </div>
                     </div>
+                </div>
+
+                <div v-if="searchResults.length == 0 && search.length > 0" class="text-gray-400 my-5">
+                    No results for this search
                 </div>
             </div>
         </div>
-    </div>
     </div>
 </template>
