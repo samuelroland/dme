a45f288d853d561a0634bdcadce297b5f1a51f20
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-10 23:00:22 +0200

    fix(search): make test_large_search_on_mdn_content_can_find_a_several_headings() test pass

--- app/core/tests/large_search.rs
+++ app/core/tests/large_search.rs
@@ -1,6 +1,6 @@
 use common::regression::check_possible_regression;
 use dme_core::search::disk::DiskResearcher;
-use dme_core::search::search::Researcher;
+use dme_core::search::search::{ResearchResult, Researcher};
 use pretty_assertions::{assert_eq, assert_ne};
 use std::fs::{read_to_string, write};
 use std::path::{Path, PathBuf};
@@ -22,7 +22,7 @@ fn test_large_search_on_mdn_content_can_find_multiple_match_for_generic_keyword(
     let results = disk_search.search(search, 50, None);
     let stats = disk_search.stats();
     dbg!(&stats);
-    disk_search.print_index_stats();
+    // disk_search.print_index_stats();
     assert!(stats.markdown_paths_count > 13740); // as of 2025-06-10
     assert!(stats.headings_count > 17100); // number of UNIQUE headings as of 2025-06-10
     dbg!(&results);
@@ -81,12 +81,13 @@ fn test_large_search_on_mdn_content_can_find_a_several_headings() {
     let mut disk_search = DiskResearcher::new(repos.to_str().unwrap().to_string());
     disk_search.start();
     let search = "Array constructor with"; // should match 2 headings
-    let results = disk_search.search(search, 20, None);
+    let mut results = disk_search.search(search, 20, None);
     assert_eq!(
         results.len(),
         2,
         "Results should have only 2 results, contains\n{results:?}"
     );
+    results.sort_by(|a, b| a.title.cmp(&b.title));
     assert_eq!(
         results[0].title,
         Some("Array constructor with a single parameter".to_string())
@@ -95,7 +96,7 @@ fn test_large_search_on_mdn_content_can_find_a_several_headings() {
         "content/files/en-us/web/javascript/reference/global_objects/array/array/index.md"
     ));
     assert_eq!(
-        results[0].title,
+        results[1].title,
         Some("Array constructor with multiple parameters".to_string())
     );
     assert!(results[1].path.ends_with(
@@ -108,18 +109,24 @@ fn test_large_search_on_mdn_content_is_fuzzy_ordered_matching() {
     let repos = clone_mdn_content();
     let mut disk_search = DiskResearcher::new(repos.to_str().unwrap().to_string());
     disk_search.start();
-    let search = "Array constructor single"; // should fuzzy match the
-    let results = disk_search.search(search, 20, None);
-    assert_eq!(
-        results.len(),
-        1,
-        "Results should have only one result, contains\n{results:?}"
-    );
-    assert_eq!(
-        results[0].title,
-        Some("Array constructor with a single parameter".to_string())
-    );
-    assert!(results[0].path.ends_with(
-        "content/files/en-us/web/javascript/reference/global_objects/array/array/index.md"
-    ));
+    let patterns = vec![
+        "arra c single",
+        "Array constructor single",
+        "constructor sgle array",
+    ];
+    for pattern in patterns {
+        let results = disk_search.search(pattern, 20, None);
+        assert_eq!(
+            results.len(),
+            1,
+            "Results should have only one result, contains\n{results:?}"
+        );
+        assert_eq!(
+            results[0].title,
+            Some("Array constructor with a single parameter".to_string())
+        );
+        assert!(results[0].path.ends_with(
+            "content/files/en-us/web/javascript/reference/global_objects/array/array/index.md"
+        ));
+    }
 }
