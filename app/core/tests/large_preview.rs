99d1038305b27d72a15c62701c864931a9ebff23
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-11 23:55:45 +0200

    fix(preview): fix test test_large_markdown_preview_with_codes_gives_same_result()

--- app/core/tests/large_preview.rs
+++ app/core/tests/large_preview.rs
@@ -4,13 +4,16 @@ use std::fs::{read_to_string, write};
 use std::path::{Path, PathBuf};
 
 use dme_core::markdown_to_highlighted_html;
-use dme_core::util::setup::generate_large_markdown_with_codes;
+use dme_core::util::setup::{
+    generate_large_markdown_with_codes, install_all_grammars_in_local_target_folder,
+};
 use dme_core::*;
 use regex::Replacer;
 mod common;
 
 #[test]
 fn test_large_markdown_preview_with_codes_gives_same_result() {
+    install_all_grammars_in_local_target_folder();
     for i in [1, 2, 5] {
         let test_id = format!("large-preview-{}", i);
         let path = generate_large_markdown_with_codes(i, 100);
