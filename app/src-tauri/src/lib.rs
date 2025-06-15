0fcdd3bf020a929c12c462aade17c58f453dd578
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-15 18:33:55 +0200

    refactor(tauri): refactor commands in sub files

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,4 +1,9 @@
+mod commands;
+
+use crate::commands::grammars::get_grammars_list;
+use crate::commands::search::run_search;
 use std::{
+    collections::{HashMap, HashSet},
     env::current_dir,
     path::{Path, PathBuf},
     sync::{
@@ -9,9 +14,17 @@ use std::{
     time::{Duration, Instant},
 };
 
+use commands::{
+    grammars::{grammars_folder, install_grammar, remove_grammar},
+    home::get_app_info,
+    preview::open_markdown_file,
+};
 use dme_core::{
     markdown_to_highlighted_html,
-    preview::preview::Html,
+    preview::{
+        preview::Html, proposed_grammars::PROPOSED_GRAMMAR_SOURCES,
+        tree_sitter_grammars::TreeSitterGrammarsManager,
+    },
     search::{
         disk::DiskResearcher,
         search::{Progress, ResearchResult, Researcher},
@@ -19,122 +32,11 @@ use dme_core::{
 };
 use serde::Serialize;
 use tauri::{AppHandle, Emitter};
-
-#[derive(Serialize)]
-struct AppInfo {
-    version: String,
-}
-
-#[tauri::command]
-fn get_app_info() -> AppInfo {
-    AppInfo {
-        version: env!("CARGO_PKG_VERSION").to_owned(),
-    }
-}
-
-#[tauri::command]
-/// Open given Markdown file or the default one provided as argument
-/// or none otherwise
-fn open_markdown_file(mut path: String) -> Result<Option<Html>, String> {
-    println!("{path:?}");
-    if path.is_empty() {
-        path = {
-            let args: Vec<String> = std::env::args().collect();
-            if args.len() < 2 {
-                String::default()
-            } else {
-                args[1].clone()
-            }
-        }
-    }
-
-    if path.is_empty() {
-        Ok(None)
-    } else if PathBuf::from(&path).exists() {
-        Ok(Some(markdown_to_highlighted_html(&path)?))
-    } else {
-        Err(format!("File {path} doesn't exist !").to_string())
-    }
-}
-
 use tauri::{Builder, Manager};
 
 struct AppData {
     disk_researcher: Mutex<DiskResearcher>,
     search_stream_receiver: Mutex<Option<Receiver<ResearchResult>>>,
-}
-
-#[tauri::command]
-fn run_search(app: AppHandle, search: String) -> Result<String, String> {
-    thread::spawn(move || {
-        println!("Searching {search} ");
-        let state = app.state::<AppData>();
-        let mut ds = state.disk_researcher.lock().unwrap();
-        if !ds.has_started() {
-            println!("INDEXING START");
-            ds.start();
-        }
-        drop(ds);
-
-        let mut existing_rx = state.search_stream_receiver.lock().unwrap();
-
-        let (tx, rx) = mpsc::channel::<ResearchResult>();
-        *existing_rx = Some(rx);
-        drop(existing_rx);
-
-        // Start the thread in parallel to avoid blocking
-        let app_arc = Arc::new(app);
-
-        let app_arced = app_arc.clone();
-        thread::spawn(move || {
-            let state = app_arced.state::<AppData>();
-            let mut progress = Progress(0);
-            while !progress.is_done() {
-                let guard = state.disk_researcher.lock().unwrap();
-                progress = guard.progress();
-                println!("Progress in indexing {}", progress.0);
-                drop(guard);
-                if progress.is_done() {
-                    println!("INDEXING DONE");
-                    break;
-                } else {
-                    println!("Progress in indexing {}", progress.0);
-                }
-                sleep(Duration::from_millis(10));
-            }
-        });
-        let app_arced_local = app_arc.clone();
-        // Span a thread to listen on rx events
-        thread::spawn(move || {
-            let state = app_arced_local.state::<AppData>();
-            let now = Instant::now();
-            loop {
-                let rx_guard = state.search_stream_receiver.lock().unwrap();
-
-                if let Some(rx) = rx_guard.as_ref() {
-                    if let Ok(res) = rx.recv() {
-                        println!("{:?}", res.path);
-                        // print!(".");
-                        app_arced_local.emit("search-match", res).unwrap();
-                        sleep(Duration::from_millis(10));
-                    } else {
-                        println!("End of search after {:?}", now.elapsed());
-                        break;
-                    }
-                } else {
-                    break;
-                }
-                drop(rx_guard);
-            }
-        });
-
-        let app_arced_local = app_arc.clone();
-
-        let state = app_arc.state::<AppData>();
-        let ds = state.disk_researcher.lock().unwrap();
-        ds.search(&search, 10, Some(tx.clone()));
-    });
-    Ok("started".to_string())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -144,7 +46,11 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![
             get_app_info,
             run_search,
-            open_markdown_file
+            open_markdown_file,
+            get_grammars_list,
+            install_grammar,
+            remove_grammar,
+            grammars_folder
         ])
         .setup(|app| {
             let home_dir = etcetera::home_dir()
